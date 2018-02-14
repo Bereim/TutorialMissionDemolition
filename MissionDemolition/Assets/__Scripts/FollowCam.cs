f3f7d089279fdd8ffe17e3e6097a1f4363c40e91
Bereim <35741860+Bereim@users.noreply.github.com>
2018-02-14 16:08:23 -0600

    Added a Castle and Follow Cam

--- MissionDemolition/Assets/__Scripts/FollowCam.cs
+++ MissionDemolition/Assets/__Scripts/FollowCam.cs
@@ -17,10 +17,22 @@ public class FollowCam : MonoBehaviour {
 	}
 
 	private void FixedUpdate() {
-		if (POI == null) return;
+        //if (POI == null) return;
+        //Vector3 destination = POI.transform.position;
 
-		Vector3 destination = POI.transform.position;
-		destination.x = Mathf.Max(minXY.x, destination.x);
+        Vector3 destination;
+        if (POI == null) destination = Vector3.zero;
+        else {
+            destination = POI.transform.position;
+            if (POI.CompareTag("Projectile")) {
+                if (POI.GetComponent<Rigidbody>().IsSleeping()) {
+                    POI = null;
+                    return;
+                }
+            }
+        }
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
 		destination.y = Mathf.Max(minXY.y, destination.y);
 		destination = Vector3.Lerp(transform.position, destination, easing);
 		destination.z = camZ;
